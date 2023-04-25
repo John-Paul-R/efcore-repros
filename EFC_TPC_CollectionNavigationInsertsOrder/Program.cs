b29202d491ed6acb313b3de91824cff630ab30b6
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2023-04-24 22:11:48 -0400

    deps & initial layout

--- EFC_TPC_CollectionNavigationInsertsOrder/Program.cs
+++ EFC_TPC_CollectionNavigationInsertsOrder/Program.cs
@@ -1,3 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
-Console.WriteLine("Hello, World!");
+namespace EFSampleApp;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using (var db = new MyContext())
+        {
+            // Recreate database
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            // Seed database
+
+
+            db.SaveChanges();
+        }
+
+        using (var db = new MyContext())
+        {
+            // Run queries
+            var query = db.Blogs.ToList();
+        }
+        Console.WriteLine("Program finished.");
+    }
+}
+
+
+public class MyContext : DbContext
+{
+    private static ILoggerFactory ContextLoggerFactory
+        => LoggerFactory.Create(b =>
+        {
+            b
+                .AddConsole()
+                .AddFilter("", LogLevel.Debug);
+        });
+
+    // Declare DBSets
+    public DbSet<Blog> Blogs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Select 1 provider
+        optionsBuilder
+            // .UseSqlite(@"Server=(localdb)\mssqllocaldb;Database=_ModelApp;Trusted_Connection=True;Connect Timeout=5;ConnectRetryCount=0")
+            .UseSqlite("filename=_modelApp.db")
+            //.UseInMemoryDatabase(databaseName: "_modelApp")
+            //.UseCosmos("https://localhost:8081", @"C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", "_ModelApp")
+            .EnableSensitiveDataLogging()
+            .UseLoggerFactory(ContextLoggerFactory);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure model
+    }
+}
+
+public class Blog
+{
+    public int Id { get; set; }
+}
