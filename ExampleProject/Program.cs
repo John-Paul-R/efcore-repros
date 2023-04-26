f05b58c24c8ddea569c8251797df2f1fd413fa45
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2023-04-26 19:15:39 -0400

    Add the repro

--- ExampleProject/Program.cs
+++ ExampleProject/Program.cs
@@ -39,7 +39,34 @@ public class Program
 
             // -- YOUR REPRO HERE --
 
-            db.SaveChanges();
+            var martialOrMagicSkills = db.Skills
+                .Where(s => s is MartialSkill || s is MagicSkill)
+                .ToList();
+
+            // The above query yields the following db command:
+            // SELECT "t"."Id", "t"."Name", "t"."RequiredDivinity", "t"."RunicName", "t"."HasStrike", "t"."Discriminator"
+            // FROM (
+            //     SELECT "d"."Id", "d"."Name", "d"."RequiredDivinity", NULL AS "RunicName", NULL AS "HasStrike", 'DivineSkill' AS "Discriminator"
+            //     FROM "DivineSkills" AS "d"
+            //     UNION ALL
+            //     SELECT "m"."Id", "m"."Name", NULL AS "RequiredDivinity", "m"."RunicName", NULL AS "HasStrike", 'MagicSkill' AS "Discriminator"
+            //     FROM "MagicSkills" AS "m"
+            //     UNION ALL
+            //     SELECT "m0"."Id", "m0"."Name", NULL AS "RequiredDivinity", NULL AS "RunicName", "m0"."HasStrike", 'MartialSkill' AS "Discriminator"
+            //     FROM "MartialSkills" AS "m0"
+            // ) AS "t"
+            // WHERE "t"."Discriminator" IN ('MartialSkill', 'MagicSkill')
+
+            // This query targets all 3 tables, instead of just the two that match the types being filtered on.
+            // This is inefficient and does not match the behavior observed when filtering on a single type. e.g.
+
+            var magicSkills = db.Skills
+                .Where(s => s is MagicSkill)
+                .ToList();
+
+            // yields the following (which appears more correct):
+            // SELECT "m"."Id", "m"."Name", NULL AS "RequiredDivinity", "m"."RunicName", NULL AS "HasStrike", 'MagicSkill' AS "Discriminator"
+            // FROM "MagicSkills" AS "m"
         }
         Console.WriteLine("Program finished.");
     }
